9440ab8453b1c69312d2af31985eb8f925b3d5e2
Rafael Fernandes <rafaellemosfernandes@gmail.com>
2021-08-21 19:58:46 -0300

    Create color selector

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -18,3 +18,37 @@ function fixCase (word) {
   }
   return `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`
 }
+
+
+const form = document.querySelector('[data-js="form"]')
+const select = document.createElement('select')
+const colors = ['black', 'blue', 'red', 'green', 'grey']
+const colorsContainer = document.createElement('div')
+colorsContainer.style.display = 'flex'
+
+colors.forEach(color => {
+  const option = document.createElement('option')
+  option.value = color
+  option.textContent = color
+  select.appendChild(option)
+})
+
+select.addEventListener('change', (e) => {
+colorsContainer.innerHTML = ''
+
+  Array.from(e.target.selectedOptions).forEach(option => {
+    const div = document.createElement('div')
+    div.style.width = '100px'
+    div.style.height = '100px'
+    div.style.background = option.value
+    colorsContainer.appendChild(div)
+  })
+})
+
+
+
+select.setAttribute('multiple', '')
+form.appendChild(select)
+document.body.appendChild(colorsContainer)
+
+console.log(select)
