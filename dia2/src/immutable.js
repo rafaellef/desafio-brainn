1c62ecd64ccd2c38c3aaee114054f35f0479135d
Rafael Fernandes <rafaellemosfernandes@gmail.com>
2021-08-21 11:58:15 -0300

    import and fix object

--- dia2/src/immutable.js
+++ dia2/src/immutable.js
@@ -5,10 +5,12 @@ const john = {
   hobbies: ['Surf', 'Design'],
 }
 
-const jane = john
+const jane = {
+  ...john,
 
-jane.name = 'Jane'
-jane.hobbies.push('MuayThai', 'Programming')
+name: 'Jane',
+hobbies: john.hobbies.concat('MuayThai', 'Programming'),
+}
 
 console.log('John:', john)
 console.log('Jane:', jane)
